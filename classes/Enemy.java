5d20666d7736b7dbda80b824ac842011672d645e
Muhitdin Yahya <monkeydjack0@mail.ru>
2020-01-15 16:16:44 +0100

    new enemies

--- classes/Enemy.java
+++ classes/Enemy.java
@@ -1,2 +1,24 @@
-public class Enemy {
+import java.util.*;
+
+public abstract class Enemy {
+    protected int hp;
+    protected int atk;
+    protected int def;
+    public Enemy()
+    {
+
+    }
+    public int getDmg()
+    {
+        return atk;
+    }
+    public int getHp()
+    {
+        return hp;
+    }
+    public int getDef()
+    {
+        return def;
+    }
+
 }
--- classes/Player.java
+++ classes/Player.java
@@ -1,5 +1,21 @@
-public class Player {
+import java.util.HashMap;
 
-    
+public class Player {
+    private String Name;
+    private HashMap<String, Weapon> Weapons = new HashMap<>();
+    private HashMap<String, Armor> Armors = new HashMap<>();
+    private Stats Stats;
+
+
+
+    public void EquipWeapon(){
+
+    }
+    public void EquipArmor(){
+
+    }
+    public void Refresh(){
+
+    }
 
 }
